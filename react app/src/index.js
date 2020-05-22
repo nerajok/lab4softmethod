df02f1e466f898eb2ae3f0f0419176828108168c
unknown <nkumarabu@translucentcomputing.com>
2020-05-22 14:18:58 -0400

    wipped out

--- react app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import registerServiceWorker from './registerServiceWorker';
-import RegisterForm from './registrationForm/RegisterForm';
-
-
-ReactDOM.render(<RegisterForm /> , document.getElementById('root'));
-
-// registerServiceWorker();
